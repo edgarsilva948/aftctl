38dd677fa7dd001f01defd55d4b4ef0ba05a479d
Edgar Silva <edgarsilva948@gmail.com>
2023-09-16 01:57:54 -0300

    fix: refactoring the local command to gocyclo

--- cmd/local/cmd.go
+++ cmd/local/cmd.go
@@ -77,6 +77,12 @@ var Cmd = &cobra.Command{
 
 // Run executes the local command
 func Run(cmd *cobra.Command, argv []string) {
+
+	// client initialization with AFT Credentials
+	log.Info("initializing AWS Client using AFT Account credentials")
+	awsClient := aws.NewClient("")
+	ssmClient := awsClient.GetSSMClient()
+
 	// defining the S3 key for the local execution
 	var tfS3Key string
 
@@ -92,86 +98,112 @@ func Run(cmd *cobra.Command, argv []string) {
 		os.Exit(1)
 	}
 
-	// client initialization with AFT Credentials
-	log.Info("Initializing AWS Client using AFT Account credentials")
-	awsClient := aws.NewClient("")
+	// Define an array of SSM parameter keys that we need to fetch.
+	ssmKeys := []string{
+		aftMgmtAccountID,
+		tfDistribution,
+		ctMgmtRegion,
+		aftAdminRoleName,
+		aftExecutionRoleName,
+		tfBackendRegion,
+		tfKmsKeyID,
+		tfDynamoDBTableName,
+		tfS3BucketID,
+	}
 
-	aftMgmtAccountIDParam, err := aws.GetSSMParameter(awsClient.GetSSMClient(), aftMgmtAccountID)
+	// Fetch the SSM parameters based on the keys defined above.
+	params := getSSMParameters(ssmClient, ssmKeys)
+
+	// Check if fetching the parameters was successful.
 	if err != nil {
-		log.Fatalf("Failed to get SSM Parameter: %v", err)
+		log.Fatalf("failed to get SSM Parameters: %v", err)
 		os.Exit(1)
 	}
 
-	tfDistributionParam, err := aws.GetSSMParameter(awsClient.GetSSMClient(), tfDistribution)
-	if err != nil {
-		log.Fatalf("Failed to get SSM Parameter: %v", err)
+	// Validate that each parameter key has a value.
+
+	// Check for DynamoDB table name parameter.
+	tfDynamoDBTableNameParam, ok := params[tfDynamoDBTableName]
+	if !ok {
+		log.Fatalf("tfDynamoDBTableName is missing from SSM parameters")
 		os.Exit(1)
 	}
 
-	ctMgmtRegionParam, err := aws.GetSSMParameter(awsClient.GetSSMClient(), ctMgmtRegion)
-	if err != nil {
-		log.Fatalf("Failed to get SSM Parameter: %v", err)
+	// Check for Management Account ID parameter.
+	aftMgmtAccountIDParam, ok := params[aftMgmtAccountID]
+	if !ok {
+		log.Fatalf("aftMgmtAccountID is missing from SSM parameters")
 		os.Exit(1)
 	}
 
-	aftAdminRoleNameParam, err := aws.GetSSMParameter(awsClient.GetSSMClient(), aftAdminRoleName)
-	if err != nil {
-		log.Fatalf("Failed to get SSM Parameter: %v", err)
-		os.Exit(1)
-	}
-	aftExecutionRoleNameParam, err := aws.GetSSMParameter(awsClient.GetSSMClient(), aftExecutionRoleName)
-	if err != nil {
-		log.Fatalf("Failed to get SSM Parameter: %v", err)
+	// Check for Admin Role Name parameter.
+	aftAdminRoleNameParam, ok := params[aftAdminRoleName]
+	if !ok {
+		log.Fatalf("aftAdminRoleName is missing from SSM parameters")
 		os.Exit(1)
 	}
 
-	tfBackendRegionParam, err := aws.GetSSMParameter(awsClient.GetSSMClient(), tfBackendRegion)
-	if err != nil {
-		log.Fatalf("Failed to get SSM Parameter: %v", err)
+	// Check for Distribution Type parameter.
+	tfDistributionParam, ok := params[tfDistribution]
+	if !ok {
+		log.Fatalf("tfDistribution is missing from SSM parameters")
 		os.Exit(1)
 	}
 
-	tfKmsKeyIDParam, err := aws.GetSSMParameter(awsClient.GetSSMClient(), tfKmsKeyID)
-	if err != nil {
-		log.Fatalf("Failed to get SSM Parameter: %v", err)
+	// Check for Management Region parameter.
+	ctMgmtRegionParam, ok := params[ctMgmtRegion]
+	if !ok {
+		log.Fatalf("ctMgmtRegion is missing from SSM parameters")
 		os.Exit(1)
 	}
 
-	tfDynamoDBTableNameParam, err := aws.GetSSMParameter(awsClient.GetSSMClient(), tfDynamoDBTableName)
-	if err != nil {
-		log.Fatalf("Failed to get SSM Parameter: %v", err)
+	// Check for Backend Region parameter.
+	tfBackendRegionParam, ok := params[tfBackendRegion]
+	if !ok {
+		log.Fatalf("tfBackendRegion is missing from SSM parameters")
 		os.Exit(1)
 	}
 
-	tfS3BucketIDParam, err := aws.GetSSMParameter(awsClient.GetSSMClient(), tfS3BucketID)
-	if err != nil {
-		log.Fatalf("Failed to get SSM Parameter: %v", err)
+	// Check for Execution Role Name parameter.
+	aftExecutionRoleNameParam, ok := params[aftExecutionRoleName]
+	if !ok {
+		log.Fatalf("aftExecutionRoleName is missing from SSM parameters")
+		os.Exit(1)
+	}
+
+	// Check for S3 Bucket ID parameter.
+	tfS3BucketIDParam, ok := params[tfS3BucketID]
+	if !ok {
+		log.Fatalf("tfS3BucketID is missing from SSM parameters")
+		os.Exit(1)
+	}
+
+	// Check for KMS Key ID parameter.
+	tfKmsKeyIDParam, ok := params[tfKmsKeyID]
+	if !ok {
+		log.Fatalf("tfKmsKeyID is missing from SSM parameters")
 		os.Exit(1)
 	}
 
 	// check if the current directory is aft-account-customizations or aft-global-customizations
 	pwd, err := os.Getwd()
 	if err != nil {
-		fmt.Println("Error getting current directory:", err)
+		fmt.Println("error getting current directory:", err)
 		return
 	}
 
-	// check if the current directory is aft-account-customizations or aft-global-customizations and defining the S3 key for the local execution
-	if strings.Contains(pwd, "aft-account-customizations") {
-		tfS3Key = fmt.Sprintf("%s-aft-account-customizations/terraform.tfstate", args.targetAccount)
-	} else if strings.Contains(pwd, "aft-global-customizations") {
-		tfS3Key = fmt.Sprintf("%s-aft-global-customizations/terraform.tfstate", args.targetAccount)
-	} else {
-		fmt.Println("Run aftctl local from the aft-account-customizations or aft-global-customizations repository.")
+	// Call the getTFS3Key function to determine the appropriate S3 key based on the current directory.
+	tfS3Key, err = getTFS3Key(pwd, args.targetAccount)
+	if err != nil {
 		return
 	}
 
 	// setting up the AWS profile for the AFT Account using the user current credentials
 	if err := profile.SetupProfile(awsClient.GetSTSClient(), aftMgmtAccountIDParam, aftAdminRoleNameParam, "AWSAFT-Session"); err != nil {
-		fmt.Println("Error setting up profile", err)
+		fmt.Println("error setting up profile", err)
 	}
 
-	log.Infof("Successfully set up AWS profile %s-%s", aftMgmtAccountIDParam, aftAdminRoleNameParam)
+	log.Infof("successfully set up AWS profile %s-%s", aftMgmtAccountIDParam, aftAdminRoleNameParam)
 
 	// Assuming the AFT Admin Role in the AFT Account
 	profileVariable := aftMgmtAccountIDParam + "-" + aftAdminRoleNameParam
@@ -184,31 +216,136 @@ func Run(cmd *cobra.Command, argv []string) {
 	// Set the AWS_PROFILE environment variable
 	err = os.Setenv("AWS_PROFILE", profileVariable)
 	if err != nil {
-		log.Fatalf("Error setting env var AWS_PROFILE: %v", err)
+		log.Fatalf("error setting env var AWS_PROFILE: %v", err)
 	}
 
+	// calling the function to process Jinja files
+	processJinjaFiles(
+		tfDistributionParam,
+		ctMgmtRegionParam,
+		tfBackendRegionParam,
+		aftMgmtAccountIDParam,
+		aftExecutionRoleNameParam,
+		tfS3BucketIDParam,
+		tfS3Key,
+		tfDynamoDBTableNameParam,
+		tfKmsKeyIDParam,
+	)
+
+	// Generate the .gitignore file
+	gitIgnoreGenerated := gitignore.GenerateGitIgnore()
+
+	// Check the result of the .gitignore generation
+	if gitIgnoreGenerated {
+		log.Info(".gitignore successfully generated")
+	} else {
+		log.Fatalf("error generating .gitignore file")
+	}
+
+	// calling the function to execute Terraform command
+	log.WithField("command", args.terraformCommand).Info("executing Terraform command")
+	executeTerraformCommand(args.terraformCommand, accessKey, secretKey, sessionToken)
+
+}
+
+func getSSMParameters(client aws.SSMClient, paramKeys []string) map[string]string {
+	params := make(map[string]string)
+
+	for _, key := range paramKeys {
+		param, err := aws.GetSSMParameter(client, key)
+		handleError(err, "failed to get SSM Parameter")
+		params[key] = param
+	}
+
+	return params
+}
+
+func handleError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %v", msg, err)
+		os.Exit(1)
+	}
+}
+
+func executeTerraformCommand(terraformCommand, accessKey, secretKey, sessionToken string) {
+	commandWithArgs := strings.Fields(terraformCommand)
+	terraformCmd := exec.Command("terraform", commandWithArgs...)
+	terraformCmd.Env = append(os.Environ(),
+		"AWS_ACCESS_KEY_ID="+accessKey,
+		"AWS_SECRET_ACCESS_KEY="+secretKey,
+		"AWS_SESSION_TOKEN="+sessionToken,
+	)
+
+	var stdout, stderr bytes.Buffer
+	terraformCmd.Stdout = &stdout
+	terraformCmd.Stderr = &stderr
+
+	err := terraformCmd.Run()
+	handleError(err, "cmd.Run() failed")
+
+	if err != nil {
+		log.Fatalf("cmd.Run() failed: %s\nStderr: %s", err, stderr.String())
+	}
+
+	fmt.Printf("output:\n%s\n", stdout.String())
+}
+
+// Define function to set tfS3Key based on the current directory
+func getTFS3Key(pwd string, targetAccount string) (string, error) {
+	var tfS3Key string
+
+	// Check if the current directory is "aft-account-customizations" and set tfS3Key accordingly
+	if strings.Contains(pwd, "aft-account-customizations") {
+		tfS3Key = fmt.Sprintf("%s-aft-account-customizations/terraform.tfstate", targetAccount)
+	} else if strings.Contains(pwd, "aft-global-customizations") {
+		// Check if the current directory is "aft-global-customizations" and set tfS3Key accordingly
+		tfS3Key = fmt.Sprintf("%s-aft-global-customizations/terraform.tfstate", targetAccount)
+	} else { // If the directory is neither, return an error
+		log.Errorf("Run aftctl local from the aft-account-customizations or aft-global-customizations repository.")
+		return "", fmt.Errorf("invalid directory")
+	}
+
+	return tfS3Key, nil
+}
+
+func processJinjaFiles(
+	tfDistributionParam string,
+	ctMgmtRegionParam string,
+	tfBackendRegionParam string,
+	aftMgmtAccountIDParam string,
+	aftExecutionRoleNameParam string,
+	tfS3BucketIDParam string,
+	tfS3Key string,
+	tfDynamoDBTableNameParam string,
+	tfKmsKeyIDParam string,
+) error {
+
+	// Read all files with the extension ".jinja"
 	files, err := filepath.Glob("*.jinja")
 	if err != nil {
-		fmt.Println("Error reading jinja files:", err)
-		return
+		return fmt.Errorf("error reading jinja files: %v", err)
 	}
 
 	for _, f := range files {
+		// Read the jinja template file
 		input, err := os.ReadFile(f)
 		if err != nil {
-			fmt.Println("Error reading file:", f, err)
+			fmt.Println("error reading file:", f, err)
 			continue
 		}
 
+		// Parse the jinja template
 		template, err := pongo2.FromString(string(input))
 		if err != nil {
-			fmt.Println("Error parsing template:", err)
+			fmt.Println("error parsing template:", err)
 			continue
 		}
 
+		// Define constants and generate timestamp
 		partition := "aws"
 		timestamp := time.Now().Format(time.RFC3339)
 
+		// Execute the template with context
 		output, err := template.Execute(pongo2.Context{
 			"timestamp":             timestamp,
 			"tf_distribution_type":  tfDistributionParam,
@@ -222,49 +359,17 @@ func Run(cmd *cobra.Command, argv []string) {
 			"kms_key_id":            tfKmsKeyIDParam,
 		})
 		if err != nil {
-			fmt.Println("Error executing template:", err)
+			fmt.Println("error executing template:", err)
 			continue
 		}
 
+		// Write the output to a new file with the same name but different extension
 		outputFile := fmt.Sprintf("./%s.tf", strings.TrimSuffix(f, ".jinja"))
 		if err := os.WriteFile(outputFile, []byte(output), 0644); err != nil {
-			fmt.Println("Error writing output file:", err)
+			fmt.Println("error writing output file:", err)
 			continue
 		}
 	}
 
-	gitIgnoreGenerated := gitignore.GenerateGitIgnore()
-
-	if !gitIgnoreGenerated {
-		log.Fatalf("error generating .gitignore file")
-	}
-
-	if gitIgnoreGenerated {
-
-		log.Info(".gitignore successfully generated")
-
-	}
-
-	log.WithField("command", args.terraformCommand).Info("Executing Terraform command")
-
-	commandWithArgs := strings.Fields(args.terraformCommand)
-	terraformCmd := exec.Command("terraform", commandWithArgs...)
-	terraformCmd.Env = append(os.Environ(),
-		"AWS_ACCESS_KEY_ID="+accessKey,
-		"AWS_SECRET_ACCESS_KEY="+secretKey,
-		"AWS_SESSION_TOKEN="+sessionToken,
-	)
-
-	var stdout, stderr bytes.Buffer
-	terraformCmd.Stdout = &stdout
-	terraformCmd.Stderr = &stderr
-
-	err = terraformCmd.Run()
-
-	if err != nil {
-		log.Fatalf("cmd.Run() failed: %s\nStderr: %s", err, stderr.String())
-	}
-
-	fmt.Printf("Output:\n%s\n", stdout.String())
-
+	return nil
 }
